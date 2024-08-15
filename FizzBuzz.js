a0aecd6edb3b743b2311592a805f0cb3ac90fafa
Name <102274923+Name-shitty-github-profile@users.noreply.github.com>
2024-08-15 03:06:55 -0400

    Update FizzBuzz.js
    Easy fix, optimized

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -1,12 +1,13 @@
-//mini-jeux fizz et buzz
 for(let i = 1; i <= 100; i++) {
-    if (i % 3 === 0 && i % 5 === 0) {
-        alert("fizzbuzz");
-    } else if (i % 3 === 0) {
-        alert("fizz");
-    } else if (i % 5 === 0) {
-        alert("buzz");
-    } else {
-        alert(i);
+    let word = "";
+    if (i % 3 === 0) {
+        word += "Fizz";
+    } 
+    if (i % 5 === 0) {
+        word += "Buzz"
+    } 
+    if (!word) { 
+        word = i;
     }
+    alert(word);
 }
